af4dc2fd48f4289811d78cbcd2b4ded6c439ff72
tincher <joel.ewig@gmail.com>
2018-07-21 14:30:37 +0200

    bigbuy support added, 2 connection tests

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import configparser
 import mws
+import requests
 
 config = configparser.ConfigParser()
 config.read('../account.ini')
@@ -10,7 +11,16 @@ seller_id = manne_conf['seller_id']
 secret_key = manne_conf['secret_key']
 marketplace_de = 'A1PA6795UKMFR9'
 
-
+# mws test
 products_api = mws.Products(access_key, secret_key, seller_id, region='DE')
 products = products_api.list_matching_products(marketplaceid=marketplace_de, query='tv')
-print(products.parsed)
+print('amazon geht: ' + str(products.response.status_code == 200))
+
+# bigbuy test
+headers = {'Authorization': 'Bearer ' + manne_conf['bigbuy_api_key']}
+test_url = '/rest/catalog/categories.json?isoCode=de&_format=json'
+url = '/rest/shipping/carriers.json?_format=json'
+production_api = 'https://api.bigbuy.eu'
+sandbox_api = 'https://api.sandbox.bigbuy.eu'
+r = requests.get(production_api + url, headers=headers)
+print('bigbuy geht: ' + str(r.status_code == 200))
