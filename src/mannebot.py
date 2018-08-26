9bcba5edda5f2a46853850ddfd1ee217c6a33f62
tincher <joel.ewig@stud.uni-bamberg.de>
2018-08-26 21:18:51 +0200

    product-list refreshment; looking for skus in both datasets

--- src/mannebot.py
+++ src/mannebot.py
@@ -1,8 +1,5 @@
-import requests
-import mws
-import datetime
+import mws, requests, datetime, json
 from time import sleep
-import csv
 
 
 class ManneBot:
@@ -40,14 +37,57 @@ class ManneBot:
             if report_list.response.status_code == 200 and r.status_code == 200:
                 print('Connections successful')
                 self.running = True
+            else:
+                if report_list.response.status_code != 200:
+                    print('Connection to Amazon unsuccessful')
+                if r.status_code != 200:
+                    print('Connection to BigBuy unsuccessful')
 
+        connection_test()
+
+    # ------------------------------------------------------------------------------------------------------------------
     def refresh_product_list(self):
+        try:
+            amazon_sku_list = self.get_amazon_listing_skus()
+            bb_sku_list = self.get_bb_listing_skus()
+            self.product_list = self.get_matching_skus(amazon_sku_list, bb_sku_list)
+        except RefreshmentError as e:
+            print('Products could not be refreshed due to an Error: ', e.value)
+        else:
+            print('Products successfully refreshed')
+
+    def get_bb_listing_skus(self):
+        bb_catalog_url = self.bb_url + '/rest/catalog/products.json'
+        bb_catalog = json.loads(requests.get(bb_catalog_url, headers=self.bb_header).text)
+        result = list(map(lambda x: x["sku"], bb_catalog))
+        if not len(result) >= 0:
+            raise RefreshmentError('BigBuy: Catalog not receivable')
+        return result
+
+    def get_amazon_listing_skus(self):
         report_request = self.reports_api.request_report(report_type='_GET_MERCHANT_LISTINGS_DATA_')
-        fromdate = datetime.datetime.now() - datetime.timedelta(hours=5)
-        sleep(30)
-        report_list = self.reports_api.get_report_list(fromdate=fromdate)
-        newest_report_meta = list(
-            filter(lambda x: x.ReportType == '_GET_MERCHANT_LISTINGS_DATA_', report_list.parsed.ReportInfo))[0]
-        report = self.reports_api.get_report(newest_report_meta.ReportId)
-        self.product_list = report.parsed.decode('iso-8859-1').split('\n')
-        print('Products successfully refreshed')
+        if report_request.response.status_code == 200:
+            starting_date = datetime.datetime.now() - datetime.timedelta(hours=5)
+            sleep(60)
+            report_list = self.reports_api.get_report_list(fromdate=starting_date)
+            newest_report_meta_list = list(
+                filter(lambda x: x.ReportType == '_GET_MERCHANT_LISTINGS_DATA_', report_list.parsed.ReportInfo))
+            if len(newest_report_meta_list) <= 0:
+                raise RefreshmentError('Amazon: No Merchant Listing Data')
+            report = self.reports_api.get_report(newest_report_meta_list[0].ReportId)
+            parsed_report = report.parsed.decode('iso-8859-1').split('\n')
+            return list(map(lambda x: x.split('\t')[3] if not len(x.split('\t')) < 3 else 0, parsed_report))
+
+    # ------------------------------------------------------------------------------------------------------------------
+    @staticmethod
+    def get_matching_skus(amazon_skus, bb_skus):
+        return list(set([x for x in amazon_skus if x in bb_skus]))
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+class RefreshmentError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
